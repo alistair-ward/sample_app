34669e9f8283f5fb77319b48fed8906ff0c002b1
Alistair Ward <alistair@fred.gen.nz>
2011-02-03 20:13:06 -0800

    user model with passwords and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Alistair Ward"
+  user.email                 "alistair@fred.gen.nz"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
